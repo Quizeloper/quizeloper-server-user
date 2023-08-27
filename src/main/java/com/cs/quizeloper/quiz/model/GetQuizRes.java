7b0860a8ec76a993feec159b5694fe95f4f5bcbb
codusl100 <codusl100@naver.com>
2023-08-27 16:17:06 +0900

    #9 feat : 엔티티 수정 맞춰 DTO 변경

--- src/main/java/com/cs/quizeloper/quiz/model/GetQuizRes.java
+++ src/main/java/com/cs/quizeloper/quiz/model/GetQuizRes.java
@@ -2,21 +2,25 @@ package com.cs.quizeloper.quiz.model;
 
 import com.cs.quizeloper.quiz.entity.QuizType;
 import com.cs.quizeloper.quiz.entity.QuizUnit;
+import com.cs.quizeloper.quiz.entity.QuizUnitList;
 import com.cs.quizeloper.quiz.entity.Stack;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class GetQuizRes {
+    private Long quizId;
     private String title;
     private QuizType type;
     private Stack stackUnit;
-    private Long quizUnitId;
+    private List<String> quizUnitLists;
     private boolean likeYN;
 
 }
