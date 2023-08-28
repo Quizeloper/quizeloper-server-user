ecf9e125d013a304c733e4c80b6592cbdf73c75b
chaerlo127 <monynony0203@gmail.com>
2023-08-28 20:11:27 +0900

    #10 feat: passwordEncoder bean 생성

--- src/main/java/com/cs/quizeloper/global/config/WebSecurityConfig.java
+++ src/main/java/com/cs/quizeloper/global/config/WebSecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -18,6 +20,12 @@ import java.util.List;
 @EnableWebSecurity
 public class WebSecurityConfig implements WebMvcConfigurer {
     private final LoginResolver loginResolver;
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
