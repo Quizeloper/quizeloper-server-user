8487e5a8bef3d1547c87bbe3c8db01e6467084a6
chaerlo127 <monynony0203@gmail.com>
2023-08-28 16:34:33 +0900

    #11 fix: 서버 접근 시 Please sign in 화면 접근 에러 해결

--- src/main/java/com/cs/quizeloper/global/config/WebSecurityConfig.java
+++ src/main/java/com/cs/quizeloper/global/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @RequiredArgsConstructor
@@ -14,6 +15,8 @@ public class WebSecurityConfig{
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
                 .cors(AbstractHttpConfigurer::disable)
+                .headers(h -> h.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .formLogin(AbstractHttpConfigurer::disable)
                 .build();
     }
 }
