f639df06a1b784e886709cdebb8ce10df353da5b
chaerlo127 <monynony0203@gmail.com>
2023-08-30 17:25:15 +0900

    #23 feat: 문의하기 API

--- src/main/java/com/cs/quizeloper/inquiry/controller/InquiryController.java
+++ src/main/java/com/cs/quizeloper/inquiry/controller/InquiryController.java
@@ -1,7 +1,15 @@
 package com.cs.quizeloper.inquiry.controller;
 
+import com.cs.quizeloper.global.exception.BaseResponse;
+import com.cs.quizeloper.global.exception.BaseResponseStatus;
+import com.cs.quizeloper.global.resolver.Account;
+import com.cs.quizeloper.global.resolver.UserInfo;
+import com.cs.quizeloper.inquiry.dto.PostInquiryReq;
 import com.cs.quizeloper.inquiry.service.InquiryService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class InquiryController {
     private final InquiryService inquiryService;
+
+    // 문의하기
+    @PostMapping("")
+    public BaseResponse<BaseResponseStatus> postInquiry(@Account UserInfo userInfo, @RequestBody @Valid PostInquiryReq inquiryReq){
+        inquiryService.postInquiry(userInfo.getId(), inquiryReq);
+        return new BaseResponse<>(BaseResponseStatus.SUCCESS);
+    }
 }
--- src/main/java/com/cs/quizeloper/inquiry/service/InquiryService.java
+++ src/main/java/com/cs/quizeloper/inquiry/service/InquiryService.java
@@ -1,9 +1,25 @@
 package com.cs.quizeloper.inquiry.service;
 
+import com.cs.quizeloper.global.entity.BaseStatus;
+import com.cs.quizeloper.global.exception.BaseException;
+import com.cs.quizeloper.global.exception.BaseResponseStatus;
+import com.cs.quizeloper.inquiry.Repository.InquiryRepository;
+import com.cs.quizeloper.inquiry.dto.PostInquiryReq;
+import com.cs.quizeloper.inquiry.entity.Inquiry;
+import com.cs.quizeloper.user.Repository.UserRepository;
+import com.cs.quizeloper.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class InquiryService {
+    private final UserRepository userRepository;
+    private final InquiryRepository inquiryRepository;
+
+    // 문의하기 생성
+    public void postInquiry(Long userId, PostInquiryReq inquiryReq) {
+        User user = userRepository.findByIdAndStatus(userId, BaseStatus.ACTIVE).orElseThrow(() -> new BaseException(BaseResponseStatus.USER_NOT_FOUND));
+        inquiryRepository.save(Inquiry.toDto(inquiryReq, user));
+    }
 }
