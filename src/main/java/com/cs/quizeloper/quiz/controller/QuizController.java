5f268352588654a6daca77a21def115ecd9b9189
codusl100 <codusl100@naver.com>
2023-08-30 19:37:59 +0900

    #15 refactor: UserInfo 적용 & 퀴즈 문제 유형 반환 메서드 분리

--- src/main/java/com/cs/quizeloper/quiz/controller/QuizController.java
+++ src/main/java/com/cs/quizeloper/quiz/controller/QuizController.java
@@ -2,6 +2,8 @@ package com.cs.quizeloper.quiz.controller;
 
 import com.cs.quizeloper.global.exception.BaseException;
 import com.cs.quizeloper.global.exception.BaseResponse;
+import com.cs.quizeloper.global.resolver.Account;
+import com.cs.quizeloper.global.resolver.UserInfo;
 import com.cs.quizeloper.quiz.model.GetQuizRes;
 import com.cs.quizeloper.quiz.service.QuizService;
 import lombok.RequiredArgsConstructor;
@@ -24,7 +26,7 @@ public class QuizController {
      */
     @ResponseBody
     @GetMapping("")
-    public BaseResponse<Page<GetQuizRes>> getQuizList(@RequestParam(name = "size", defaultValue = "16") Integer size,
+    public BaseResponse<Page<GetQuizRes>> getQuizList(@Account UserInfo userInfo, @RequestParam(name = "size", defaultValue = "16") Integer size,
                                                       @RequestParam(name = "page", defaultValue = "0") Integer page) {
         try {
             if (page < 0) {
@@ -34,7 +36,7 @@ public class QuizController {
                 return new BaseResponse<>(PAGE_SIZE_COUNT_UNDER);
             }
             PageRequest pageRequest = PageRequest.of(page, size);
-            Page<GetQuizRes> quizPage = quizService.getQuizList(pageRequest);
+            Page<GetQuizRes> quizPage = quizService.getQuizList(userInfo.getId(), pageRequest);
             return new BaseResponse<>(quizPage);
         } catch (BaseException exception){
             return new BaseResponse<>(exception.getStatus());
--- src/main/java/com/cs/quizeloper/quiz/service/QuizService.java
+++ src/main/java/com/cs/quizeloper/quiz/service/QuizService.java
@@ -1,5 +1,6 @@
 package com.cs.quizeloper.quiz.service;
 
+import com.cs.quizeloper.global.resolver.UserInfo;
 import com.cs.quizeloper.quiz.Repository.QuizLikeRepository;
 import com.cs.quizeloper.quiz.Repository.QuizRepository;
 import com.cs.quizeloper.quiz.entity.*;
@@ -24,27 +25,29 @@ public class QuizService {
 
     // 퀴즈 전체 목록 조회
     @Transactional
-    public Page<GetQuizRes> getQuizList(PageRequest pageRequest) {
+    public Page<GetQuizRes> getQuizList(Long userId, PageRequest pageRequest) {
         Page<Quiz> pagingQuiz = quizRepository.findAllByStatus(ACTIVE, pageRequest);
-        Page<Long> quizLikes = quizLikeRepository.findAllByUserAndStatus(1L, ACTIVE, pageRequest);
+        Page<Long> quizLikes = quizLikeRepository.findAllByUserAndStatus(userId, ACTIVE, pageRequest);
 
-        // List 형식의 QuizUnit 값 String 배열에 담기
-        List<String> quizString = pagingQuiz.getContent().stream()
-                .flatMap(quiz -> quiz.getQuizUnitList().stream().map(QuizUnitList::getQuizUnit))
-                .map(quizUnit -> String.valueOf(quizUnit.getUnit()))
-                .collect(Collectors.toList());
+        List<String> quizString = getQuizUnitList(pagingQuiz.getContent());
 
-        // 최종적으로 DTO에 반환
         List<GetQuizRes> quizResList = pagingQuiz.getContent().stream()
                 .map(quiz -> new GetQuizRes(quiz.getId(), quiz.getTitle(), quiz.getType(), quiz.getStackUnit(), quizString,
                         checkUserLikesQuiz(quizLikes.toList(), quiz)))
                 .collect(Collectors.toList());
-
         return new PageImpl<>(quizResList, pageRequest, pagingQuiz.getTotalElements());
     }
 
     // 퀴즈 좋아요 확인 여부
     private boolean checkUserLikesQuiz(List<Long> quizLike, Quiz quiz) {
         return quizLike.contains(quiz.getId());
+    }
+
+    // 퀴즈 문제 유형 반환
+    private List<String> getQuizUnitList(List<Quiz> quizList){
+        return quizList.stream()
+                .flatMap(quiz -> quiz.getQuizUnitList().stream().map(QuizUnitList::getQuizUnit))
+                .map(quizUnit -> String.valueOf(quizUnit.getUnit()))
+                .collect(Collectors.toList());
     }
 }
