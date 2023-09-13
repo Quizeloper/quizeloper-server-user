ef2b4d36bae36a8cef27b80e138b391cc9ad61af
codusl100 <codusl100@naver.com>
2023-09-13 11:34:44 +0900

    #40 feat: Repository 파일 추가

--- src/main/java/com/cs/quizeloper/quiz/Repository/QuizHistoryRepository.java
+++ src/main/java/com/cs/quizeloper/quiz/Repository/QuizHistoryRepository.java
@@ -1,9 +1,13 @@
 package com.cs.quizeloper.quiz.Repository;
 
+import com.cs.quizeloper.global.entity.BaseStatus;
 import com.cs.quizeloper.quiz.entity.QuizHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface QuizHistoryRepository extends JpaRepository<QuizHistory, Long> {
+    Optional<QuizHistory> findFirstByQuizIdAndUserIdAndStatusOrderByCreatedDateDesc(long quizId, long userId, BaseStatus ACTIVE);
 }
